eb681bf4e3a5169a7e1815c1ad85d56538ae7081
Fred Wrodie <fred@mildlyupset.com>
2021-06-19 15:40:24 +0200

    api5

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 
 
+let serverURL;
+
+    fetch(".netlify/functions/api")
+    .then(response => response.json())
+    .then(json => {
+        var faunadb = window.faunadb;
+var q = faunadb.query;
+var client = new faunadb.Client({
+    secret: json.api,
+    domain: "db.fauna.com",
+    scheme: "https",
+});
+
+    })
+function key(){
+    console.log(serverURL);
+}
+    
+
