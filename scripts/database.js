0777e61a49436e9f347365c7b9611845bd2ed82b
Fred Wrodie <fred@mildlyupset.com>
2021-06-25 18:16:14 +0200

    newstuff

---
+++ scripts/database.js
@@ -0,0 +1,108 @@
+// var faunadb;
+// var q;
+// var client;
+
+// let serverURL;
+
+//     fetch(".netlify/functions/api")
+//     .then(response => response.json())
+//     .then(json => {
+//         faunadb = window.faunadb;
+//         q = faunadb.query;
+//         client = new faunadb.Client({
+//             secret: json.api,
+//             domain: "db.fauna.com",
+//             scheme: "https",
+//         });
+
+//     })
+    
+// // Load Fauna Stuff
+// let NoCalls = 0;
+// let NoPosts = 0;
+// var faunadb = window.faunadb;
+// var q = faunadb.query;
+// var client = new faunadb.Client({
+//     secret: "fnAEH9qaMKACAZUacat_8gPzT9yf_m85U7fy8kdb",
+//     domain: "db.fauna.com",
+//     scheme: "https",
+// });
+
+
+// // Load DataBase from Fauna
+// function GetDataBase() {
+//     NoCalls ++;
+//     logger('GetDataBase',`Calls: ${NoCalls}, Posts: ${NoPosts}`);
+//     client
+//         .query(
+//             q.Paginate(q.Match(q.Index("NewDemQuestions")))
+
+//             )
+
+//         .then((ret) => {
+//             questions = ret.data;
+//             logger('GetDataBase',questions);
+//         })
+//         .catch((err) => console.error("Error: %s", err));
+// }
+
+// // Update Fauna DataBase
+// function UpdateDataBase(Num, Answer) {
+//     NoPosts ++;
+//     logger('GetDataBase',`Calls: ${NoCalls}, Posts: ${NoPosts}`);
+//     if(Answer == 'yes'){
+//         client
+//         .query(
+//             q.Map(q.Paginate(q.Match(q.Index('NewNumbers'),Num)),
+//             q.Lambda(
+//                 'X',
+//                 q.Update(
+//                   q.Var('X'),
+//                   {
+//                     data: {
+//                         Total: q.Add(
+//                         q.Select(['data', 'Total'], q.Get(q.Var('X'))),
+//                         1
+//                       ),
+//                       Yes: q.Add(
+//                         q.Select(['data', 'Yes'], q.Get(q.Var('X'))),
+//                         1
+//                       )
+//                     }
+//                   }
+//                 )
+//             )
+//         )
+//         )
+//         .then((ret) => logger('UpdateDataBase',ret))
+//         .catch((err) => console.error("Error: %s", err));
+//     }else{
+//         client
+//         .query(
+//             q.Map(q.Paginate(q.Match(q.Index('NewNumbers'),Num)),
+//             q.Lambda(
+//                 'X',
+//                 q.Update(
+//                   q.Var('X'),
+//                   {
+//                     data: {
+//                         Total: q.Add(
+//                         q.Select(['data', 'Total'], q.Get(q.Var('X'))),
+//                         1
+//                       ),
+//                       No: q.Add(
+//                         q.Select(['data', 'No'], q.Get(q.Var('X'))),
+//                         1
+//                       )
+//                     }
+//                   }
+//                 )
+//             )
+//         )
+//         )
+//         .then((ret) => logger('UpdateDataBase',ret))
+//         .catch((err) => console.error("Error: %s", err));
+//     }
+    
+// }
+
---
+++ scripts/mainscript.js
@@ -0,0 +1,24 @@
+
+var Logger = true;
+
+// When Page Loads
+document.onload = pageSetUp();
+
+// Page Load Function
+function pageSetUp() {
+    generateStartingGrid();
+    loadLotties();
+    let readywidth = document.getElementById("no47").offsetHeight;
+    wait(readywidth, 0);
+    //FadeIn();
+}
+
+//Logger
+function logger(func,thing){
+    if(Logger == true){
+    console.log(`${func} whispers:`);
+    console.log(thing);
+    }
+}
+
+
